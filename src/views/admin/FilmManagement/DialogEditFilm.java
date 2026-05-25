96f0c48f94fb062c52e98b578a84488d98a03e98
rgn-cream <reginanf7@gmail.com>
2026-05-25 08:19:57 +0700

    manage structure

--- src/views/admin/FilmManagement/DialogEditFilm.java
+++ src/views/admin/FilmManagement/DialogEditFilm.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
  */
-package views.admin.FilmManagement;
+package views.admin;
 
 import controllers.FilmController;
 import models.Film;
